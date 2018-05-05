8bf786c3710f5d939fdece8f31af6cb618e36293
Adult <Adult@DESKTOP-3ULAN2D>
2018-05-05 10:50:28 +0900

    성인

--- 99_DLeague/src/Seong.java
+++ 99_DLeague/src/Seong.java
@@ -2,7 +2,7 @@
 public class Seong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubsdsdsd
 		System.out.println("hello");
 	}
 
