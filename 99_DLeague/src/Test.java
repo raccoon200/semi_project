12f723d3a59453886cdb92ad5990034c8de94611
DESKTOP-43K9OEC\USER <raccoon200@naver.com>
2018-05-05 23:44:10 +0900

    불필요한 파일 제거

--- 99_DLeague/src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor 성인이테스트sss
-		System.out.println("sddㅇdddㅇㅇㅇㅇsssds");
-	}
-
-}
--- 99_DLeague/src/test2.java
+++
@@ -1,8 +0,0 @@
-
-public class test2 {
-
-	public test2() {
-		// TODO Auto-generated constructor stubkkk
-	}
-
-}
