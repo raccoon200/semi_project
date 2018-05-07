0c8bed84c3b3a2a2a113a3da04034acb9c32593b
DESKTOP-43K9OEC\USER <raccoon200@naver.com>
2018-05-07 22:17:58 +0900

    DB sequence 생성 query문

---
+++ 99_DLeague/sql/dleague_create_sequence.sql
@@ -0,0 +1,87 @@
+create sequence seq_member_register_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_notice_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_board_region_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_board_free_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_board_free_comment_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_complain_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_delete_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_team_register_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_activity_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_game_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
+
+create sequence seq_wait_no
+start with 1
+increment by 1
+nominvalue
+nomaxvalue
+nocycle
+nocache;
