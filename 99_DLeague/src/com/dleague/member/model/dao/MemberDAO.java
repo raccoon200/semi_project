b54ca431c06510647dfee5fbdbeb73c78b64711b
user1 <user1@user1-PC>
2018-05-10 09:28:59 +0900

    sdsa

--- 99_DLeague/src/com/dleague/member/model/dao/MemberDAO.java
+++ 99_DLeague/src/com/dleague/member/model/dao/MemberDAO.java
@@ -153,7 +153,7 @@ private Properties prop = new Properties();
 		String query = prop.getProperty("memberInfoUpdate");
 		PreparedStatement pstmt = null;
 		try {
-			
+			//sss
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, member.getPassword());
 			pstmt.setString(2, member.getUserName());
