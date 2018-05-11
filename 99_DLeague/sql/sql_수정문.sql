e588579604374d3fd9891bbb85b51ea036c408fc
ydusr <ydusr@DESKTOP-HK80DNS>
2018-05-11 10:10:31 +0900

    sql문 수정 추가

--- 99_DLeague/sql/sql_수정문.sql
+++ 99_DLeague/sql/sql_수정문.sql
@@ -17,6 +17,8 @@ ALTER TABLE ACTIVITY
 		
 		
 --05.11
+
+--지역게시판
 drop table board_region_comment;
 drop table board_region;
 
@@ -52,4 +54,35 @@ CREATE TABLE BOARD_REGION_COMMENT (
     constraint pk_board_region_comment primary key(board_region_comment_no),
     constraint fk_board_region_ref foreign key(board_region_ref) references board_region(board_region_no) on delete cascade,
     constraint fk_board_region_comment_ref foreign key(board_region_comment_ref) references board_region_comment(board_region_comment_no) on delete cascade
+);
+
+--자유게시판
+drop table board_free_comment;
+drop table board_free;
+
+CREATE TABLE BOARD_FREE (
+	BOARD_FREE_NO NUMBER NOT NULL, /* �����Խ��� ��ȣ */
+	BOARD_FREE_TITLE VARCHAR2(50) NOT NULL, /* �����Խ��� ���� */
+	BOARD_FREE_WRITER VARCHAR2(15) NOT NULL, /* �����Խ��� �ۼ��� */
+	BOARD_FREE_CONTENT CLOB NOT NULL, /* �����Խ��� ���� */
+	BOARD_FREE_DATE DATE NOT NULL, /* �Խó�¥ */
+	ORIGINAL_FILE_NAME VARCHAR2(300), /* ���������̸� */
+	RENAMED_FILE_NAME VARCHAR2(300), /* �ٲ������̸� */
+	STATUS VARCHAR2(1) NOT NULL, /* ��뿩�� */
+	COUNT NUMBER NOT NULL, /* ��ȸ�� */
+    constraint pk_board_free primary key(board_free_no)
+);
+
+CREATE TABLE BOARD_FREE_COMMENT (
+	BOARD_FREE_COMMENT_NO NUMBER NOT NULL, /* �����Խ��Ǵ�۹�ȣ */
+	BOARD_FREE_COMMENT_LEVEL NUMBER NOT NULL, /* ���/���� */
+	BOARD_FREE_COMMENT_WRITER VARCHAR2(15) NOT NULL, /* �ۼ��� */
+	BOARD_FREE_COMMENT_CONTENT VARCHAR2(2000) NOT NULL, /* ���� */
+	BOARD_FREE_REF NUMBER, /* �θ� �Խñ� ��ȣ */
+	BOARD_FREE_COMMENT_REF NUMBER, /* �θ� ���� ��ȣ */
+	BOARD_FREE_COMMENT_DATE DATE NOT NULL, /* �ۼ��� */
+    constraint pk_free_free_comment primary key(board_free_comment_no),
+    constraint fk_free_free_ref foreign key(board_free_ref) references board_free(board_free_no) on delete cascade,
+    constraint fk_free_free_comment_ref foreign key(board_free_comment_ref) references board_free_comment(board_free_comment_no) on delete cascade
+
 );
