24a149e0ece3c97d51cf243a2a781bab9fb9c06c
BaeSaint <happy3980@naver.com>
2018-05-16 09:35:06 +0900

    커밋

--- 99_DLeague/src/com/dleague/memberTeam/model/service/MemberTeamService.java
+++ 99_DLeague/src/com/dleague/memberTeam/model/service/MemberTeamService.java
@@ -59,15 +59,8 @@ public class MemberTeamService {
 		return result;
 	}
 
-	public int MemberTeamMandateLeader(String leader, String choose) {
+	public int MemberTeamMandate(String leader, String choose) {
 		Connection conn = getConnection();
-<<<<<<< HEAD
-		MemberTeamDAO memberTeamDAO = new MemberTeamDAO();
-		memberTeamDAO.MemberTeamMandateLeader(conn, choose);
-		int result = memberTeamDAO.MemberTeamMandateMember(conn, leader);
-		if(result>0) commit(conn);
-		else rollback(conn);
-=======
 		int result = 0;
 		MemberTeamDAO memberteamDAO = new MemberTeamDAO();
 		result = memberteamDAO.memberTeamMandateLeader(conn, choose);
@@ -78,7 +71,6 @@ public class MemberTeamService {
 		if(result>0) commit(conn);
 		else rollback(conn);
 		
->>>>>>> branch 'master' of https://github.com/raccoon200/semi_project.git
 		close(conn);
 		return result;
 	}
